cd820e80f224d568fd8cbad9b8b4c3683599e214
Hugh Haworth <hughelliotclyde@gmail.com>
2020-08-25 18:57:37 +1200

    Built grid and colour button

--- src/App.js
+++ src/App.js
@@ -10,10 +10,10 @@ function CurrentColour({x,colour}){
   return <div style={styles} className="current-colour"></div>;
 }
 
-function ColourBox(){
+function ColourBox({setColour,colour}){
   const [isDown,setIsDown] = useState(false);
   const [x,setX]= useState(0);
-  const [colour,setColour]= useState("rgb(255,0,0)");
+ 
   const [currentWidth,setCurrentWidth]= useState(window.innerWidth);
 
   useEffect(()=>{
@@ -56,9 +56,37 @@ function ColourBox(){
   return <div className="colour-box-cont"><button onMouseDown={onMouseDown}  className="colour-box"></button><CurrentColour x={x} colour={colour}/></div>;
 }
 
+function GridBuilder({colour}){
+  const [grid,setGrid] = useState(createGreyGrid());
+    
+  return(
+    <div className="grid-builder"><button style={{backgroundColor:colour}}>Generate hue grid</button>
+    <div className="grid">
+      {grid.map(row=>{
+        const newRow = row.map(item=>{
+          return <div className="grid-item" style={{backgroundColor:item}}></div>
+        });
+        return newRow;
+      })}
+    </div>
+    </div>
+  );
+}
+
 function App() {
-  
-  return <ColourBox/>;
+  const [colour,setColour]= useState("rgb(255,0,0)");
+  return <div className="app-container"><ColourBox setColour={setColour} colour={colour}/><GridBuilder colour={colour}/></div>;
 }
 
 export default App;
+
+function createGreyGrid(){
+  const empty =new Array(10);
+  empty.fill(new Array(10));
+  const result = empty.map(
+    (row)=>{
+      return(row.fill("rgb(220,220,220)"))
+    }
+  );
+  return result;
+  }
