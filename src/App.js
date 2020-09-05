d7a8d618aed072a43a51d2d0c24b298895f4ff82
Hugh Haworth <hughelliotclyde@gmail.com>
2020-09-05 14:15:43 +1200

    Added text and created accelorating selector

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, useRef } from 'react';
 import xToColour from './xToColour';
-import processGridColour from './GridUtils';
+import { processGridColour, rgbToHex, copyToClipBoard } from './GridUtils';
 
 function keepInScreen(x) {
   if (x < 0) {
@@ -12,12 +12,17 @@ function keepInScreen(x) {
   return x;
 }
 
-function ColourTab({ x, colour, onMouseDown,onKeyDown,onKeyUp }) {
+function keepInSpeedLimit(count){
+  var returnCount =Math.ceil(count/4)
+  return (returnCount>10)?10:returnCount;
+}
+
+function ColourTab({ x, colour, onMouseDown, onKeyDown, onKeyUp }) {
   const styles = {
     transform: `translate(calc(${x}px - 50%), 0px)`,
     backgroundColor: colour
   };
-  return <button style={styles} onKeyDown={onKeyDown} onKeyUp={onKeyUp} onMouseDown={onMouseDown} id="colour-tab"className="colour-tab"></button>;
+  return <button style={styles} onKeyDown={onKeyDown} onKeyUp={onKeyUp} onMouseDown={onMouseDown} id="colour-tab" className="colour-tab"></button>;
 }
 
 function ColourBox({ setColour, colour }) {
@@ -26,10 +31,12 @@ function ColourBox({ setColour, colour }) {
   const xRef = useRef();
   const stopBudgeInterval = useRef(() => { });
   const keyDown = useRef();
+  const keyHeldCount =useRef(0);
   const [currentWidth, setCurrentWidth] = useState(window.innerWidth);
+  
 
   useEffect(() => {
-    xRef.current=x;
+    xRef.current = x;
     window.addEventListener('mouseup', onMouseUp);
     window.addEventListener('mousemove', onMouseMove);
     window.addEventListener('resize', onResize);
@@ -42,21 +49,23 @@ function ColourBox({ setColour, colour }) {
   });
 
   function budge(direction) {
-    
-    if(direction==="ArrowLeft"){
-      setX(keepInScreen(xRef.current-1));
+
+    if (direction === "ArrowLeft") {
+      setX(keepInScreen(xRef.current - keepInSpeedLimit(keyHeldCount.current)));
     }
-    if(direction==="ArrowRight"){
-      setX(keepInScreen(xRef.current+1));
+    if (direction === "ArrowRight") {
+      setX(keepInScreen(xRef.current + keepInSpeedLimit(keyHeldCount.current)));
     }
     setColour(xToColour(xRef.current));
     xRef.current = keepInScreen(x);
+    keyHeldCount.current=keyHeldCount.current+1;
   }
   function onKeyUp(e) {
     const key = e.key;
     if (key === keyDown.current) {
       stopBudgeInterval.current();
       keyDown.current = null;
+      keyHeldCount.current=0;
     }
   }
   function onKeyDown(e) {
@@ -104,21 +113,67 @@ function GridBuilder({ colour, gridColour, onGenerateHueGrid }) {
   const grid = createGrid(gridColour);
   return (
     <div className="grid-builder">
-      <a href="#grid"  className="hue-button" style={{ backgroundColor: colour }} onClick={onGenerateHueGrid}>Generate hue grid</a>
+      <a href="#grid" className="hue-button" id="hue-button" style={{ backgroundColor: colour }} onClick={onGenerateHueGrid}>Generate hue grid</a>
       <div className="grid-spacer"></div>
       <div id="grid" className="grid">
         {
           grid.map((row, rowIndex) => {
-            const newRow = row.map((item, columnIndex) => {
-              return <button key={`grid-item-row${rowIndex}-column-${columnIndex}`} className="grid-item"
-                style={{ backgroundColor: processGridColour(item, columnIndex, rowIndex) }}></button>
-            });
+            const newRow = row.map(
+              (item, columnIndex) => {
+                return <GridItem
+                  key={`grid-item-row${rowIndex}-column-${columnIndex}`}
+                  rowIndex={rowIndex} columnIndex={columnIndex}
+                  colour={processGridColour(item, columnIndex, rowIndex)} />
+              }
+            )
             return newRow;
           })
         }
       </div>
+      <CssVariablesText colourGrid={grid} />
     </div>
+
   );
+}
+
+function GridItem({ colour }) {
+
+  function onClick(e) {
+    copyToClipBoard(rgbToHex(colour));
+  }
+
+  return (<button
+    className="grid-item"
+    style={{ backgroundColor: colour }}
+    onClick={onClick}></button>);
+}
+
+function CssVariablesText({ colourGrid }) {
+
+  const [propPrefix, setPrefix ] = useState('');
+  
+  const text = colourGrid.map((row,rowIndex) => {
+    const newRow = row.map((item,columnIndex) => {
+      const always = `-l${columnIndex}-s${9-(rowIndex)}:${rgbToHex(processGridColour(item, columnIndex, rowIndex))};\r\n`;
+      if(propPrefix===""){
+        return "-"+always;
+      }
+      else
+      return `--${propPrefix}`+always;
+    })
+    return newRow.join("");
+  }).join("");
+  
+
+  return (
+    <React.Fragment>
+  <h2>CSS Custom properties</h2>
+  <div id="css-variables-title" className="prefix-cont">
+  <label className="prefix-label" htmlFor="prefix">Add prefix?</label>
+  <input id="prefix" type="text" placeholder="enter prefix" value={propPrefix} onChange={e=>setPrefix(e.target.value)} />
+  </div>
+  <textarea id="css-variables" readOnly rows="100" className="css-variables-text" value={text}></textarea>
+  </React.Fragment>);
 }
 
 function App() {
@@ -126,12 +181,18 @@ function App() {
   const [gridColour, setGridColour] = useState("rgb(255,0,0)");
 
   function onGenerateHueGrid() {
-
     setGridColour(colour);
   }
 
   return (
     <div className="app-container">
+      <h1>Colour Grid</h1> 
+      <ol>
+        <li><a href="#colour-box">Select a hue.</a></li> 
+        <li><a href="#hue-button">Generate a grid.</a></li>
+        <li><a href="#grid">Click a square to copy the hashcode.</a></li> 
+        <li><a href="#css-variables-title">Or copy the grid as CSS custom properties (with an optional prefix).</a></li> 
+        </ol>
       <ColourBox setColour={setColour} colour={colour} />
       <GridBuilder colour={colour} gridColour={gridColour} onGenerateHueGrid={onGenerateHueGrid} />
     </div>);
--- src/GridUtils.js
+++ src/GridUtils.js
@@ -1,69 +1,91 @@
 
 
 
-  
-export default function processGridColour(colour, columnIndex,rowIndex){
 
-    function rgbToIntArray(rgb){
-        return rgb.replace(/ /g,'').slice(4, -1).split(',').map(e=>parseInt(e));   
-    }
+export function processGridColour(colour, columnIndex, rowIndex) {
 
-    function getColoursFromRgbArray(rgbArray){
-        const primary = rgbArray.findIndex(e=>e===255);
-        const zeroColour = rgbArray.findIndex(e=>e===0);
-        const thirdColour = rgbArray.findIndex((e,i)=>(i!==primary&&i!==zeroColour));
-          return {
-            primary:primary,
-            zeroColour:zeroColour,
-            thirdColour:thirdColour
-          };
-      }
-      
-      function lightenRgbBy(rgb,amount){  
-          const numberOfLightens = 6; 
-          const rgbArray = rgbToIntArray(rgb);
-          const {zeroColour,thirdColour} = getColoursFromRgbArray(rgbArray);
-      
-          const returnArray = [...rgbArray];
-          returnArray[zeroColour] = rgbArray[zeroColour]+((255/numberOfLightens)*amount);
-          returnArray[thirdColour] = Math.round(rgbArray[thirdColour] + ((255 - rgbArray[thirdColour])/numberOfLightens)*amount);
-          console.log();
-          return(`rgb(${returnArray.join()})`);
-      }
-      
-      
-
-      function darkenRgbBy(rgb,amount){
-        const numberOfDarkens = 5; 
-        const rgbArray = rgbToIntArray(rgb);
-        const {primary,thirdColour} = getColoursFromRgbArray(rgbArray);
-        const returnArray = [...rgbArray];
-      
-        returnArray[primary]= Math.round(rgbArray[primary]-((255/numberOfDarkens)*amount));
-        returnArray[thirdColour] = Math.round(rgbArray[thirdColour] - (rgbArray[thirdColour]/numberOfDarkens)*amount);
-        return(`rgb(${returnArray.join()})`);
-      }
-
-      function greyRgbBy(rgb,amount){
-        const numberOfGreys = 9; 
-        const rgbArray = rgbToIntArray(rgb);
-
-        const highest = rgbArray.indexOf(Math.max(...rgbArray));
-        const lowest = rgbArray.indexOf(Math.min(...rgbArray));
-        const endGrey = (rgbArray[highest]+rgbArray[lowest])/2;
-
-        const diffsToGrey = rgbArray.map(e=>(e-endGrey)/numberOfGreys);
-        const returnArray = rgbArray.map((e,i)=>e-(diffsToGrey[i]*amount));
-        return(`rgb(${returnArray.join()})`);
-      }
-
-  
-      if(columnIndex<4){
-        return greyRgbBy(darkenRgbBy(colour,(4-columnIndex)),rowIndex);
-      }
-      if(columnIndex>4){
-        return greyRgbBy(lightenRgbBy(colour,(columnIndex-4)),rowIndex);
-      }
-      return greyRgbBy(colour,rowIndex);
+  function getColoursFromRgbArray(rgbArray) {
+    const primary = rgbArray.findIndex(e => e === 255);
+    const zeroColour = rgbArray.findIndex(e => e === 0);
+    const thirdColour = rgbArray.findIndex((e, i) => (i !== primary && i !== zeroColour));
+    return {
+      primary: primary,
+      zeroColour: zeroColour,
+      thirdColour: thirdColour
+    };
   }
+
+  function greyRgbBy(rgb, amount) {
+    const numberOfGreys = 9;
+    const rgbArray = rgbToIntArray(rgb);
   
+    const highest = rgbArray.indexOf(Math.max(...rgbArray));
+    const lowest = rgbArray.indexOf(Math.min(...rgbArray));
+    const endGrey = (rgbArray[highest] + rgbArray[lowest]) / 2;
+  
+    const diffsToGrey = rgbArray.map(e => (e - endGrey) / numberOfGreys);
+    const returnArray = rgbArray.map((e, i) => e - Math.round((diffsToGrey[i] * amount)));
+    return (`rgb(${returnArray.join()})`);
+  }
+
+  function lightenRgbBy(rgb, amount) {
+    const numberOfLightens = 6;
+    const rgbArray = rgbToIntArray(rgb);
+    const { zeroColour, thirdColour } = getColoursFromRgbArray(rgbArray);
+
+    const returnArray = [...rgbArray];
+    returnArray[zeroColour] = rgbArray[zeroColour] + ((255 / numberOfLightens) * amount);
+    returnArray[thirdColour] = Math.round(rgbArray[thirdColour] + ((255 - rgbArray[thirdColour]) / numberOfLightens) * amount);
+    console.log();
+    return (`rgb(${returnArray.join()})`);
+  }
+
+
+
+  function darkenRgbBy(rgb, amount) {
+    const numberOfDarkens = 5;
+    const rgbArray = rgbToIntArray(rgb);
+    const { primary, thirdColour } = getColoursFromRgbArray(rgbArray);
+    const returnArray = [...rgbArray];
+
+    returnArray[primary] = Math.round(rgbArray[primary] - ((255 / numberOfDarkens) * amount));
+    returnArray[thirdColour] = Math.round(rgbArray[thirdColour] - (rgbArray[thirdColour] / numberOfDarkens) * amount);
+    return (`rgb(${returnArray.join()})`);
+  }
+
+
+  if (columnIndex < 4) {
+    return greyRgbBy(darkenRgbBy(colour, (4 - columnIndex)), rowIndex);
+  }
+  if (columnIndex > 4) {
+    return greyRgbBy(lightenRgbBy(colour, (columnIndex - 4)), rowIndex);
+  }
+  return greyRgbBy(colour, rowIndex);
+}
+
+export function rgbToHex(rgb) {
+  const [red, green, blue] = rgbToIntArray(rgb)
+  return '#' + toHex(red) + toHex(green) + toHex(blue);
+}
+
+function toHex(n) {
+  n = parseInt(n, 10);
+  if (isNaN(n)) return "00";
+  n = Math.max(0, Math.min(n, 255));
+  return "0123456789ABCDEF".charAt((n - n % 16) / 16)
+    + "0123456789ABCDEF".charAt(n % 16);
+}
+
+function rgbToIntArray(rgb) {
+  return rgb.replace(/ /g, '').slice(4, -1).split(',').map(e => parseInt(e));
+}
+
+
+export function copyToClipBoard(str) {
+  const el = document.createElement('textarea');
+  el.value = str;
+  document.body.appendChild(el);
+  el.select();
+  document.execCommand('copy');
+  document.body.removeChild(el);
+}
